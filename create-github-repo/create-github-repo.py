215a10c32ca3be211a157c52291f90e4ab616f11
Arne Sørli <81353974+arneso-ssb@users.noreply.github.com>
2022-09-21 08:59:46 +0200

    Create GitHub repo and read back stats

--- create-github-repo/create-github-repo.py
+++ create-github-repo/create-github-repo.py
@@ -2,8 +2,7 @@ import argparse
 import logging
 import sys
 
-from git import Repo
-from github import Github
+from github import Github, GithubException
 
 logging.basicConfig(
     level=logging.INFO,
@@ -16,21 +15,41 @@ logging.basicConfig(
 )
 
 
-def main(token):
+def main(token: str, repo_name: str):
     logging.info("Scanning organization for active public repos...")
+    org_name = "statisticsnorway"
     g = Github(token)
 
-    public_repos = []
-    for repo in g.get_organization("statisticsnorway").get_repos(type="public"):
-        if not repo.archived:
-            logging.info(f"{repo.full_name} is public")
-            public_repos.append(repo)
-    logging.info(f"There are {len(public_repos)} public repos")
+    org = g.get_organization(org_name)
+
+    try:
+        org.create_repo(
+            repo_name,
+            private=True,
+            auto_init=True,
+            gitignore_template="Python",
+            license_template="mit",
+            description="Test-repo for dapla-hurtigstart at #Hack4ssb 2022",
+        )
+    except GithubException:
+        logging.warning(f"Repo {repo_name} already exists.")
+    else:
+        logging.info(f"Repo {repo_name} created.")
+
+    repo = g.get_repo(f"{org_name}/{repo_name}")
+    logging.info(f"{repo.full_name=}")
+    logging.info(f"{repo.private=}")
+
+    # main_branch = repo.get_branch("main")
+    # main_branch.edit_protection()
+    # protection = main_branch.get_protection()
+    # logging.info(f"{protection}")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=sys.modules[__name__].__doc__)
     parser.add_argument("token", help="GitHub Personal Access Token")
+    parser.add_argument("repo_name", help="Name of the GitHub repo to be created")
     args = parser.parse_args()
 
-    main(args.token)
+    main(args.token, args.repo_name)
