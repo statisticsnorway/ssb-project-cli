a68e0213a24271f2e5f8f864c134094ef4e28e2f
Nicolai Stølen <nicolai.stolen@ssb.no>
2022-10-06 08:18:06 +0200

    Added unit tests for hurtigstart (app.py)

--- tests/test_hurtigstart.py
+++ tests/test_hurtigstart.py
@@ -1,20 +1,26 @@
-from dataclasses import dataclass
+"""Tests for src/ssb_project_cli/ssb_project/app.py."""
+
 from pathlib import Path
 from unittest.mock import Mock
 from unittest.mock import patch
-from webbrowser import get
 
 import pytest
 from github import BadCredentialsException
 from github import GithubException
 
+from ssb_project_cli.ssb_project.app import create_github
+from ssb_project_cli.ssb_project.app import create_project_from_template
 from ssb_project_cli.ssb_project.app import extract_name_email
 from ssb_project_cli.ssb_project.app import get_gitconfig_element
+from ssb_project_cli.ssb_project.app import get_kernels_dict
 from ssb_project_cli.ssb_project.app import is_github_repo
 from ssb_project_cli.ssb_project.app import make_git_repo_and_push
 from ssb_project_cli.ssb_project.app import mangle_url
+from ssb_project_cli.ssb_project.app import projectname_from_currfolder
 from ssb_project_cli.ssb_project.app import request_name_email
 from ssb_project_cli.ssb_project.app import set_branch_protection_rules
+from ssb_project_cli.ssb_project.app import workspace
+from ssb_project_cli.ssb_project.app import workspace_uri_from_projectname
 
 
 PKG = "ssb_project_cli.ssb_project.app"
@@ -23,16 +29,18 @@ PKG = "ssb_project_cli.ssb_project.app"
 @pytest.mark.parametrize(
     "url,mangle,expected",
     [
-        ("https://example.com", "test", "https://test@example.com"),
+        ("ssh://example.com", "test", "ssh://test@example.com"),
         ("git://github.com/etc", "user", "git://user@github.com/etc"),
     ],
 )
 def test_mangle_url(url: str, mangle: str, expected: str) -> None:
+    """Checks if mangle_url returns the correct mangled string."""
     assert mangle_url(url, mangle) == expected
 
 
 @patch(f"{PKG}.Github.get_repo")
 def test_is_github_repo(mock_get_repo: Mock) -> None:
+    """Checks if is_github_repo returns/raises expected values/errors."""
     mock_get_repo.side_effect = [
         None,
         GithubException(1, None, None),
@@ -50,6 +58,11 @@ def test_is_github_repo(mock_get_repo: Mock) -> None:
 def test_make_git_repo_and_push(
     mock_getrepo: Mock, mock_getuser: Mock, mock_repo: Mock
 ) -> None:
+    """Checks that make_git_repo_and_push works.
+
+    The git repo is mocked with 5 fake remotes to check that
+    repo.delete_remote is called the expected amount of times.
+    """
     mock_getuser.return_value = Mock(login="user")
 
     test_repo = Mock(remotes=range(5))
@@ -65,6 +78,10 @@ def test_make_git_repo_and_push(
 
 @patch(f"{PKG}.subprocess.run")
 def test_get_gitconfig_element(mock_run: Mock) -> None:
+    """Checks that get_gitconfig_element works correctly.
+
+    NOTE: The function should probably not return None. Typeguard complains.
+    """
     mock_run.side_effect = [Mock(stdout=s) for s in ["Name ", " name@email.com", ""]]
     assert get_gitconfig_element("user.name") == "Name"
     assert get_gitconfig_element("user.email") == "name@email.com"
@@ -73,6 +90,7 @@ def test_get_gitconfig_element(mock_run: Mock) -> None:
 
 @patch(f"{PKG}.subprocess.run")
 def test_extract_name_email(mock_run: Mock) -> None:
+    """Checks if extract_name_email returns the expected values."""
     mock_run.side_effect = [
         Mock(stdout=s) for s in ["Name ", " name@email.com", "Name2", ""]
     ]
@@ -82,11 +100,91 @@ def test_extract_name_email(mock_run: Mock) -> None:
 
 @patch(f"{PKG}.typer.prompt")
 def test_request_name_email(mock_prompt: Mock) -> Mock:
+    """Checks if request_name_email returns the expected values."""
     mock_prompt.side_effect = ["Name", "email@email.com"]
     assert request_name_email() == ("Name", "email@email.com")
 
 
-@patch(f"{PKG}.Github.get_repo")
-def test_set_branch_protection_rules(mock_getrepo: Mock) -> None:
+@patch(f"{PKG}.Github")
+def test_set_branch_protection_rules(mock_github: Mock) -> None:
+    """Checks if set_branch_protection_rules works.
+
+    Just cheks whether the token is given as an argument to
+    Github() and repo name to Github().get_repo.
+    """
     set_branch_protection_rules("token", "repo")
-    assert "repo" in mock_getrepo.call_args[0][0]
+    assert "token" in mock_github.call_args[0][0]
+
+
+@patch(f"{PKG}.extract_name_email")
+def test_create_project_from_template(mock_extract: Mock, tmp_path: Path) -> None:
+    """Checks if create_project_from_template works for a temporary path.
+
+    NOTE: Kind of an integration test. Could mock away subprocess.run for pure unit test.
+    """
+    mock_extract.return_value = ("Name", "email@email.com")
+    with patch(f"{PKG}.DEFAULT_REPO_CREATE_PATH", tmp_path):
+        create_project_from_template("testname", "test description")
+        project_dir = tmp_path.joinpath("testname")
+        assert project_dir.exists()
+
+        pyproject = project_dir.joinpath("pyproject.toml")
+        assert pyproject.exists()
+
+        with pytest.raises(ValueError):
+            create_project_from_template("testname", "test description")
+
+
+@patch(f"{PKG}.workspace_uri_from_projectname", return_value="")
+@patch(f"{PKG}.typer.echo")
+@patch(f"{PKG}.projectname_from_currfolder", return_value="")
+def test_workspace(
+    mock_projectname: Mock, mock_echo: Mock, mock_workspaceuri: Mock
+) -> None:
+    """Checks if workspace works as expected."""
+    workspace()
+    assert mock_echo.call_count == 3
+
+
+@patch(f"{PKG}.typer.echo", lambda x: "")
+@patch(f"{PKG}.debug_without_create_repo", False)
+@patch(f"{PKG}.Github")
+def test_create_github(mock_github: Mock) -> None:
+    """Checks if create_github works."""
+    create_github("token", "repo", "privacy", "desc")
+    assert mock_github.call_count == 1
+
+
+@patch(f"{PKG}.os")
+@patch(f"{PKG}.toml.load")
+def test_projectname_from_currfolder(mock_load: Mock, mock_os: Mock) -> None:
+    """Checks if projectname_from_currfolder works as intended."""
+    mock_os.listdir.side_effect = [["file1", "file2"], ["file3", "pyproject.toml"]]
+    mock_load.return_value = {"tool": {"poetry": {"name": "projectname"}}}
+    name = projectname_from_currfolder("")
+    assert name == "projectname"
+
+
+@patch(f"{PKG}.os.environ", {"JUPYTERHUB_USER": "test"})
+def test_workspace_uri_from_projectname() -> None:
+    """Checks that the URI returned by the workspace_uri_from_projectname is correct."""
+    project_name = "test"
+    assert (
+        workspace_uri_from_projectname(project_name)
+        == f"https://jupyter.dapla.ssb.no/user/test/lab/workspaces/{project_name}"
+    )
+
+
+@patch(f"{PKG}.subprocess.run")
+def test_get_kernels_dict(mock_run: Mock) -> None:
+    """Checks that get_kernels_dict correctly parses jupyter output."""
+    mock_run.side_effect = [
+        Mock(
+            returncode=0,
+            stdout=b"Available kernels:\n  python    /some/path\n  R    /other/path",
+        ),
+        Mock(returncode=1, stderr=b"Some error"),
+    ]
+    assert get_kernels_dict() == {"python": "/some/path", "R": "/other/path"}
+    with pytest.raises(ValueError):
+        get_kernels_dict()
