188fdb0e1c0ec8c67fcad2e72952d2b2721b3647
Anders Lunde <anders-lunde@hotmail.com>
2022-10-07 15:49:30 +0200

    Add support for cwd

--- tests/test_help.py
+++ tests/test_help.py
@@ -12,7 +12,7 @@ def test_app_build_help() -> None:
     """Checks if the cli prints help description when supplied with build --help."""
     result = runner.invoke(app, ["build", "--help"])
     assert result.exit_code == 0
-    assert "Check if Cruft recommends updating" in result.stdout
+    assert "Bygger virtuelt miljø med Poetry" in result.stdout
 
 
 def test_app_create_help() -> None:
