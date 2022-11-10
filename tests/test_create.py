3856a4c0ad6e648caa01150ecaa9fdb7a162a778
Anders Lunde <anders-lunde@hotmail.com>
2022-11-10 15:08:18 +0100

    Fix tests

--- tests/test_create.py
+++ tests/test_create.py
@@ -3,8 +3,6 @@ from unittest import TestCase
 from unittest.mock import Mock
 from unittest.mock import patch
 
-import pytest
-
 from ssb_project_cli.ssb_project.app import create
 from tests.test_app import PKG
 
@@ -13,7 +11,7 @@ from tests.test_app import PKG
 @patch(f"{PKG}.poetry_source_includes_source_name", return_value=False)
 @patch(f"{PKG}.poetry_source_add")
 @patch(f"{PKG}.poetry_source_remove")
-@patch(f"{PKG}.copytree", return_value=None)
+@patch(f"{PKG}.rmtree", return_value=None)
 @patch(f"{PKG}.poetry_install", return_value=None)
 @patch(f"{PKG}.install_ipykernel", return_value=None)
 @patch(f"{PKG}.make_and_init_git_repo", return_value=None)
@@ -27,7 +25,7 @@ class TestCreateFunction(TestCase):
         _mock_git: Mock,
         _mock_kernel: Mock,
         _mock_install: Mock,
-        mock_copy: Mock,
+        mock_rmtree: Mock,
         _mock_poetry_source_remove: Mock,
         _mock_poetry_source_add: Mock,
         _mock_poetry_source_includes_source_name: Mock,
@@ -35,40 +33,40 @@ class TestCreateFunction(TestCase):
     ) -> None:
         """Check if copytree is called when no sub functions raises an error."""
         create("test_project", "description", add_github=False)
-        assert mock_copy.call_count == 1
+        assert mock_rmtree.call_count == 0
 
-    def test_no_copy_template_error(
+    def test_rmtree_template_error(
         self,
         mock_template: Mock,
         _mock_git: Mock,
         _mock_kernel: Mock,
         _mock_install: Mock,
-        mock_copy: Mock,
+        mock_rmtree: Mock,
         _mock_poetry_source_remove: Mock,
         _mock_poetry_source_add: Mock,
         _mock_poetry_source_includes_source_name: Mock,
         _mock_running_onprem: Mock,
     ) -> None:
         """Check that copytree is not called when create_project_from_template raises an error."""
-        mock_template.side_effect = ValueError("template failing")
-        with pytest.raises(ValueError):
+        mock_template.side_effect = SystemExit(1)
+        with patch(f"{PKG}.Path.is_dir", return_value=True):
             create("test_project", "description", add_github=False)
-        assert mock_copy.call_count == 0
+        assert mock_rmtree.call_count == 1
 
-    def test_no_copy_git_error(
+    def test_rmtree_git_error(
         self,
         _mock_template: Mock,
         mock_git: Mock,
         _mock_kernel: Mock,
         _mock_install: Mock,
-        mock_copy: Mock,
+        mock_rmtree: Mock,
         _mock_poetry_source_remove: Mock,
         _mock_poetry_source_add: Mock,
         _mock_poetry_source_includes_source_name: Mock,
         _mock_running_onprem: Mock,
     ) -> None:
         """Check that copytree is not called when make_and_init_git_repo raises an error."""
-        mock_git.side_effect = ValueError("git failing")
-        with pytest.raises(ValueError):
+        mock_git.side_effect = SystemExit(1)
+        with patch(f"{PKG}.Path.is_dir", return_value=True):
             create("test_project", "description", add_github=False)
-        assert mock_copy.call_count == 0
+        assert mock_rmtree.call_count == 1
