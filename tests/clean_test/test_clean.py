eb8afdb815a2c1504510aee9392b516a92a7b797
Damir Medakovic <dam.medakovic@gmail.com>
2022-11-30 12:40:31 +0100

    Replace tests

--- tests/clean_test/test_clean.py
+++ tests/clean_test/test_clean.py
@@ -14,12 +14,10 @@ CLEAN = "ssb_project_cli.ssb_project.clean.clean"
 @patch(f"{CLEAN}.clean_venv")
 @patch(f"{CLEAN}.questionary.confirm")
 @patch(f"{CLEAN}.get_kernels_dict")
-@patch(f"{CLEAN}.subprocess.run")
 def test_clean(
-    mock_run: Mock,
     mock_kernels: Mock,
     mock_confirm: Mock,
-    _mock_clean_venv: Mock,
+    mock_clean_venv: Mock,
 ) -> None:
     """Check if the function works correctly and raises the expected errors."""
     project_name = "test-project"
@@ -29,30 +27,11 @@ def test_clean(
 
     with pytest.raises(SystemExit):
         clean_project(project_name)
+
     kernels = {project_name: "/kernel/path"}
     mock_kernels.return_value = kernels
-    mock_run.return_value = Mock(returncode=1, stderr=b"Some error")
-    with pytest.raises(SystemExit):
-        clean_project(project_name)
-
-    mock_run.return_value = Mock(
-        returncode=0,
-        stderr=f"[RemoveKernelSpec] Removed {kernels[project_name]}".encode(),
-    )
-
-    clean_project(project_name)
-
-    assert mock_run.call_count == 2
+   
+    assert mock_clean_venv.call_count == 1
 
 
-@patch(f"{CLEAN}.Path")
-@patch(f"{CLEAN}.subprocess.run")
-@patch(f"{CLEAN}.questionary")
-def test_clean_venv(confirm_mock: Mock, run_mock: Mock, path_mock: Mock) -> None:
-    confirm_mock.return_value = True
-    path_mock.is_dir.return_value = True
 
-    with pytest.raises(SystemExit):
-        clean_venv()
-
-    assert run_mock.call_count == 1
