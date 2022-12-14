81c53d3d5d486fd06aa205329ea6d6e3a0d776f0
Damir <kons-damir@ssb.no>
2022-12-14 14:10:17 +0100

    Pass in argument as string

--- src/ssb_project_cli/ssb_project/clean/clean.py
+++ src/ssb_project_cli/ssb_project/clean/clean.py
@@ -1,6 +1,7 @@
 """Clean command module."""
 import subprocess  # noqa: S404
 from pathlib import Path
+import os
 
 import questionary
 from rich import print
@@ -78,7 +79,7 @@ def clean_venv() -> None:
     ).ask()
     if confirm:
         if Path(".venv").is_dir():
-            clean_venv_cmd = "rm -rf .venv".split(" ")
+            clean_venv_cmd = "rm -rf .venv"
 
             execute_command(
                 clean_venv_cmd,
@@ -95,7 +96,8 @@ def clean_venv() -> None:
                 "Please provide the path to the ssb project you wish to delete the virtual environment for:"
             ).ask()
             if Path(f"{path}/.venv").is_dir():
-                clean_venv_cmd = f"rm -rf {path}/.venv".split(" ")
+                curr_path = os.getcwd()
+                clean_venv_cmd = f"rm -rf {curr_path}/{path}/.venv" 
 
                 execute_command(
                     clean_venv_cmd,
