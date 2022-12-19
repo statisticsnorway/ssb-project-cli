1f0fca2fbb449e853e0fd8627562ef4e98f11a7a
Damir Medakovic <dam.medakovic@gmail.com>
2022-12-19 10:52:59 +0100

    Remove check from local_repo

--- src/ssb_project_cli/ssb_project/create/local_repo.py
+++ src/ssb_project_cli/ssb_project/create/local_repo.py
@@ -34,11 +34,6 @@ def create_project_from_template(
         Path: Path of project.
     """
     project_dir = working_directory.joinpath(project_name)
-    if project_dir.exists():
-        print(
-            f"A project with name '{project_name}' already exists. Please choose another name."
-        )
-        exit(1)
 
     name, email = extract_name_email()
     if not (name and email):
