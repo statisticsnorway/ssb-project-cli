ef6a5aea56e59d995f3c402a9f8b7108c7504a4b
Damir Medakovic <dam.medakovic@gmail.com>
2023-01-03 11:07:57 +0100

    Increase test coverage

--- tests/unit/create_test/test_memory_check.py
+++ tests/unit/create_test/test_memory_check.py
@@ -1,36 +1,62 @@
 from unittest.mock import Mock
 from unittest.mock import patch
 
+import pytest
+
 from ssb_project_cli.ssb_project.create.create import is_memory_full
 
 
 CREATE = "ssb_project_cli.ssb_project.create.create"
 
 
-@patch(f"{CREATE}.psutil.virtual_memory")
-@patch(f"{CREATE}.psutil.swap_memory")
-@patch(f"{CREATE}.psutil.disk_usage")
 @patch("os.path.exists")
+@patch("psutil.disk_usage")
+@patch("psutil.swap_memory")
+@patch("psutil.virtual_memory")
 def test_is_memory_full(
-    mock_exists: Mock,
-    mock_disk_usage: Mock,
-    mock_swap_memory: Mock,
     mock_virtual_memory: Mock,
+    mock_swap_memory: Mock,
+    mock_disk_usage: Mock,
+    mock_exists: Mock,
 ) -> None:
-    # Set up the mock to return a low percentage of used virtual memory
+    # Test the case where both virtual memory and swap memory are below 95% used
     mock_virtual_memory.return_value.used = 10
     mock_virtual_memory.return_value.total = 100
-
-    # Set up the mock to return a low percentage of used swap memory
     mock_swap_memory.return_value.used = 10
     mock_swap_memory.return_value.total = 100
-
-    # Set up the mock to return True for the os.path.exists() function
     mock_exists.return_value = True
-
-    # Set up the mock to return a low percentage of used disk space
     mock_disk_usage.return_value.used = 10
     mock_disk_usage.return_value.total = 100
+    is_memory_full()
 
-    # Call the function and check that it does not exit
+    # Test the case where virtual memory is above 95% used
+    mock_virtual_memory.return_value.used = 96
+    mock_virtual_memory.return_value.total = 100
+    mock_swap_memory.return_value.used = 10
+    mock_swap_memory.return_value.total = 100
+    mock_exists.return_value = True
+    mock_disk_usage.return_value.used = 10
+    mock_disk_usage.return_value.total = 100
+    with pytest.raises(SystemExit):
+        is_memory_full()
+
+    # Test the case where swap memory is above 95% used
+    mock_virtual_memory.return_value.used = 10
+    mock_virtual_memory.return_value.total = 100
+    mock_swap_memory.return_value.used = 96
+    mock_swap_memory.return_value.total = 100
+    mock_exists.return_value = True
+    mock_disk_usage.return_value.used = 10
+    mock_disk_usage.return_value.total = 100
+    with pytest.raises(SystemExit):
+        is_memory_full()
+
+    # Test the case where the /home/jovyan/ directory does not exist
+    mock_virtual_memory.return_value.used = 10
+    mock_virtual_memory.return_value.total = 100
+    mock_swap_memory.return_value.used = 10
+    mock_swap_memory.return_value.total = 100
+    mock_exists.return_value = False
+    mock_disk_usage.return_value.used = 10
+    mock_disk_usage.return_value.total = 100
     is_memory_full()
