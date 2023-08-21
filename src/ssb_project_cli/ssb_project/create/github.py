a9c18bdb06ff6303b1f57da08178f35c33a0b903
Anders Lunde <8294494+Andilun@users.noreply.github.com>
2023-08-21 13:55:00 +0200

    Enable branch protection 'Do not allow bypassing the above settings' for created repos. (#296)
    * Set branch protection 'Do not allow bypassing the above settings' for new projects
    
    * Set default template version to v1.0.1
    
    * Run poetry update
    
    * Fix test token len issue and remove unused type ignore
    
    * Set major version requirement for mkdocs-material to ^v9 to resolve https://pyup.io/v/59587/f17/
    
    * ssb-project-cli v1.1.5

--- src/ssb_project_cli/ssb_project/create/github.py
+++ src/ssb_project_cli/ssb_project/create/github.py
@@ -204,7 +204,9 @@ def set_branch_protection_rules(
         f"{github_org_name}/{repo_name}"
     )
     repo.get_branch("main").edit_protection(
-        required_approving_review_count=1, dismiss_stale_reviews=True
+        required_approving_review_count=1,
+        dismiss_stale_reviews=True,
+        enforce_admins=True,
     )
 
 
@@ -224,7 +226,7 @@ def get_environment_specific_github_object(github_token: str) -> Github:
     if running_onprem(JUPYTER_IMAGE_SPEC):
         # CA bundle to use, supplying this fixes the onprem error "CERTIFICATE_VERIFY_FAILED"
         # verify can be boolean or string, we have to type ignore because mypy expects it to be a bool
-        return Github(github_token, verify="/etc/ssl/certs/ca-certificates.crt")  # type: ignore
+        return Github(github_token, verify="/etc/ssl/certs/ca-certificates.crt")
     else:
         return Github(github_token)
 
