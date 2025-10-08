d510aa0d965ec8bc13e957e9e930dfaf613e1401
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2025-10-08 15:57:59 +0200

    Upgrade dependencies (#517)
    * Upgrade dependencies
    
    - Remove unnecessary upper version constraints
    - Move dev dependencies out of main group
    - Update pyproject.toml format to the PyPA compatible format
    - Specify requires-python without an upper constraint
    - Fix typer rich_utils imports to be compatible with newer versions
    
    * Fix --no-verify flag
    
    The implementation of --no-verify for the create command was such that
    the flag had no effect. Invert the logic of the flag such that its
    presence actually disables verification.
    
    * Run precommit hooks
    
    * Add test to cover bug

--- noxfile.py
+++ noxfile.py
@@ -147,7 +147,14 @@ def mypy(session: Session) -> None:
     """Type-check using mypy."""
     args = session.posargs or ["src", "tests", "docs/conf.py"]
     session.install(".")
-    session.install("mypy", "pytest")
+    session.install(
+        "mypy",
+        "pytest",
+        "types-urllib3",
+        "types-requests",
+        "types-psutil",
+        "types-toml",
+    )
     session.run("mypy", *args)
     if not session.posargs:
         session.run("mypy", f"--python-executable={sys.executable}", "noxfile.py")
