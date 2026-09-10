6a9d84355ff6a5d45f5b072d6f760899aa03a035
skykanin <3789764+skykanin@users.noreply.github.com>
2026-09-10 10:44:12 +0200

    update noxfile

--- noxfile.py
+++ noxfile.py
@@ -24,7 +24,7 @@ except ImportError:
 UNIT_TESTS_PATH = "tests/unit"
 INTEGRATION_TESTS_PATH = "tests/integration"
 package = "ssb_project_cli"
-python_versions = ["3.14", "3.11"]
+python_versions = ["3.14", "3.13", "3.11"]
 nox.needs_version = ">= 2025.02.09"
 nox.options.sessions = (
     "pre-commit",
